5b2ee6436b46dddc1221c178818f206bc3e23274
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:40:09 +0300

    Add Literal control

---
+++ src/My.AspNetCore.WebForms/Controls/Literal.cs
@@ -0,0 +1,12 @@
+﻿namespace My.AspNetCore.WebForms.Controls
+{
+    public class Literal : Control
+    {
+        public string Text { get; set; }
+
+        public override void Render()
+        {
+            InnerHtml = Text;
+        }
+    }
+}
