4349d6e84ee781d08b090ea897d9066a0ab12038
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:11:16 +0300

    Add page base for web forms

---
+++ src/My.AspNetCore.WebForms/Page.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace My.AspNetCore.WebForms
+{
+    public abstract class Page
+    {
+        public event EventHandler Load;
+
+        protected internal string Content { get; set; }
+
+        protected internal HttpContext Context { get; set; }
+
+        public async virtual Task RenderAsync()
+        {
+            OnLoad();
+
+            var env = (IHostingEnvironment)Context.RequestServices
+                .GetService(typeof(IHostingEnvironment));
+            var pagesFolder = "Pages";
+            var path = Path.Combine(pagesFolder, GetType().Name + ".htm");
+            var fileInfo = env.ContentRootFileProvider
+                .GetFileInfo(path);
+
+            using (var readStream = fileInfo.CreateReadStream())
+            using (var reader = new StreamReader(readStream))
+            {
+                Content = await reader.ReadToEndAsync();
+            }
+
+            Context.Response.StatusCode = 200;
+            Context.Response.ContentType = "text/html; charset=utf-8";
+            await Context.Response.WriteAsync(Content);
+        }
+
+        protected virtual void OnLoad()
+        {
+            if (Load != null)
+            {
+                Load(this, EventArgs.Empty);
+            }
+        }
+    }
+}
