552d00f2db551ede8d075391c75841d39da5e15c
hishamco <hishamco_2007@yahoo.com>
2017-02-24 11:41:00 +0300

    Add TextBox control

---
+++ src/My.AspNetCore.WebForms/Controls/TextBox.cs
@@ -0,0 +1,12 @@
+﻿namespace My.AspNetCore.WebForms.Controls
+{
+    public class TextBox : Control
+    {
+        public string Text { get; set; }
+
+        public override void Render()
+        {
+            InnerHtml = $"<input name=\"{Id}\" type=\"text\" value=\"{Text}\" />";
+        }
+    }
+}
