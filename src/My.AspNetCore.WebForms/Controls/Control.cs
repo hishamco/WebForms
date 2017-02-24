675eee944fb2c19e6b62c2340d4bd145289f386b
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:37:48 +0300

    Add a base for web forms controls

---
+++ src/My.AspNetCore.WebForms/Controls/Control.cs
@@ -0,0 +1,11 @@
+﻿namespace My.AspNetCore.WebForms.Controls
+{
+    public abstract class Control
+    {
+        public string Id { get; set; }
+
+        public string InnerHtml { get; protected set; }
+
+        public abstract void Render();
+    }
+}
