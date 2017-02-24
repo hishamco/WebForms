c38189b1e3c3141ae257f1c4fd69f127646f7348
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:12:22 +0300

    Add AddWebForms service

---
+++ src/My.AspNetCore.WebForms/WebFormsServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using My.AspNetCore.WebForms.Infrastructure;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class WebFormsServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWebForms(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddOptions();
+
+            services.AddSingleton<IPageFactory, PageFactory>();
+
+            return services;
+        }
+    }
+}
