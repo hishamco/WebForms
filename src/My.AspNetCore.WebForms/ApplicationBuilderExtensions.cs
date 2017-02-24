de781746ae0fac5d8b5fc83e6c3f7a2aa893b887
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:13:15 +0300

    Add WebFormsMiddleware

---
+++ src/My.AspNetCore.WebForms/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using My.AspNetCore.WebForms;
+using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseWebForms(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            return app.UseMiddleware<WebFormsMiddleware>();
+        }
+    }
+}
---
+++ src/My.AspNetCore.WebForms/WebFormsMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using My.AspNetCore.WebForms.Infrastructure;
+using System;
+using System.Threading.Tasks;
+
+namespace My.AspNetCore.WebForms
+{
+    public class WebFormsMiddleware
+    {
+        private readonly IPageFactory _pageFactory;
+        private readonly RequestDelegate _next;
+
+        public WebFormsMiddleware(IPageFactory pageFactory, RequestDelegate next)
+        {
+            if (pageFactory == null)
+            {
+                throw new ArgumentNullException(nameof(pageFactory));
+            }
+
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            _pageFactory = pageFactory;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // Pesky browsers
+            if (context.Request.Path.Value.EndsWith("favicon.ico"))
+            {
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            var path = context.Request.Path.Value
+                .TrimStart(new char[] { '/' });
+
+            if (path == "")
+            {
+                path = "Index";
+            }
+
+            var page = (Page)_pageFactory.CreatePage(path);
+
+            page.Context = context;
+            await page.RenderAsync();
+        }
+    }
+}
