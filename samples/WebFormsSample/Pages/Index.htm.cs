6035c322d44205ea406696489031048200ac0ea8
hishamco <hishamco_2007@yahoo.com>
2017-02-24 09:56:53 +0300

    Use Literal control in the sample to prove that server-side rendering works

--- samples/WebFormsSample/Pages/Index.htm.cs
+++ samples/WebFormsSample/Pages/Index.htm.cs
@@ -1,9 +1,24 @@
 ﻿using My.AspNetCore.WebForms;
+using My.AspNetCore.WebForms.Controls;
+using System;
 
 namespace WebFormsSample.Pages
 {
     public class Index : Page
     {
+        private Literal litGreeting;
 
+        public Index()
+        {
+            litGreeting = new Literal();
+
+            this.Load += Page_Load;
+            this.Controls.Add(litGreeting);
+        }
+
+        private void Page_Load(object sender, EventArgs e)
+        {
+            litGreeting.Text = $"Hello, World! The time on the server is {DateTime.Now}";
+        }
     }
 }
