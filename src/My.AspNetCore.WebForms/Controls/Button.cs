7b47f546914d525e3e08501077f4e251d16c145a
hishamco <hishamco_2007@yahoo.com>
2017-02-24 11:04:34 +0300

    Add Button control

---
+++ src/My.AspNetCore.WebForms/Controls/Button.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace My.AspNetCore.WebForms.Controls
+{
+    public class Button : Control
+    {
+        public event EventHandler Click;
+
+        public string Text { get; set; }
+
+        protected virtual void OnClick()
+        {
+            Click?.Invoke(this, EventArgs.Empty);
+        }
+
+        public override void Render()
+        {
+            InnerHtml = $"<input name=\"{Id}\" type=\"submit\" value=\"{Text}\" />";
+        }
+    }
+}
