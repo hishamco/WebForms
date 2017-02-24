fcebe823c03b61179f6ff00f18ac05e2bd5b1de4
hishamco <hishamco_2007@yahoo.com>
2017-02-24 11:51:10 +0300

    Add Calculator page to WebFormsSample

---
+++ samples/WebFormsSample/Pages/Calculator.htm.cs
@@ -0,0 +1,21 @@
+﻿using My.AspNetCore.WebForms;
+using System;
+
+namespace WebFormsSample.Pages
+{
+    public partial class Calculator : Page
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToInt32(txtNumber1.Text);
+            var number2 = Convert.ToInt32(txtNumber2.Text);
+            var sum = number1 + number2;
+            litResult.Text = $"The sum is {sum}";
+        }
+    }
+}
---
+++ samples/WebFormsSample/Pages/Calculator.htm.designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WebFormsSample.Pages
+{
+    public partial class Calculator
+    {
+        private global::My.AspNetCore.WebForms.Controls.TextBox txtNumber1;
+        private global::My.AspNetCore.WebForms.Controls.TextBox txtNumber2;
+        private global::My.AspNetCore.WebForms.Controls.Button bntAdd;
+        private global::My.AspNetCore.WebForms.Controls.Literal litResult;
+
+        #region Web Form Designer generated code
+        private void InitializeComponent()
+        {
+            txtNumber1 = new global::My.AspNetCore.WebForms.Controls.TextBox();
+            txtNumber2 = new global::My.AspNetCore.WebForms.Controls.TextBox();
+            bntAdd = new global::My.AspNetCore.WebForms.Controls.Button();
+            litResult = new global::My.AspNetCore.WebForms.Controls.Literal();
+
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Id = "txtNumber1";
+            
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Id = "txtNumber2";
+
+            // 
+            // btnAdd
+            // 
+            this.bntAdd.Id = "btnAdd";
+            this.bntAdd.Text = "Add";
+            this.bntAdd.Click += btnAdd_Click;
+
+            // 
+            // litResult
+            // 
+            this.litResult.Id = "litResult";
+
+            this.Controls.Add(txtNumber1);
+            this.Controls.Add(txtNumber2);
+            this.Controls.Add(bntAdd);
+            this.Controls.Add(litResult);
+        }
+        #endregion
+    }
+}
