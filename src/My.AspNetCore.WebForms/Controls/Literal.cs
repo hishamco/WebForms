4143673a31780868a802c1e702f959c26585a5a0
hishamco <hishamco_2007@yahoo.com>
2017-02-26 21:47:27 +0300

    Add Encoded property to Literal control

--- src/My.AspNetCore.WebForms/Controls/Literal.cs
+++ src/My.AspNetCore.WebForms/Controls/Literal.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace My.AspNetCore.WebForms.Controls
@@ -7,10 +8,17 @@ namespace My.AspNetCore.WebForms.Controls
     {
         public string Text { get; set; }
 
+        public bool Encoded { get; set; } = true;
+
         public async override Task RenderAsync(TextWriter writer)
         {
             if (Text?.Length > 0)
             {
+                if (Encoded)
+                {
+                    Text = HtmlEncoder.Default.Encode(Text);
+                }
+
                 await writer.WriteAsync(Text);
             }
         }
---
+++ test/My.AspNetCore.WebForms.Tests/LiteralTest.cs
@@ -0,0 +1,28 @@
+﻿using My.AspNetCore.WebForms.Controls;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace My.AspNetCore.WebForms.Tests
+{
+    public class LiteralTest
+    {
+        [Fact]
+        public async Task DefaultTextShouldEncoded()
+        {
+            // Arrange
+            var lit = new Literal
+            {
+                Text = "<h1>Literal</h1>"
+            };
+            var writer = new StringWriter(new StringBuilder());
+
+            // Act
+            await lit.RenderAsync(writer);
+
+            // Assert
+            Assert.Equal("&lt;h1&gt;Literal&lt;/h1&gt;", lit.Text);
+        }
+    }
+}
