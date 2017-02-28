5fc717a18412c76a2f4014048b979670bb1540ef
hishamco <hishamco_2007@yahoo.com>
2017-02-28 06:18:00 +0300

    Add Title property to the Page class

--- src/My.AspNetCore.WebForms/Page.cs
+++ src/My.AspNetCore.WebForms/Page.cs
@@ -30,6 +30,8 @@ namespace My.AspNetCore.WebForms
 
         public bool IsPostBack => Context.Request.Method == "POST";
 
+        public string Title { get; set; }
+
         public async Task ExecuteAsync()
         {
             const string htmlExtension = ".htm";
