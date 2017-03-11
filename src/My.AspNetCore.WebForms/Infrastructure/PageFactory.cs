6f7dc5e0c42d52314c6017757ae0914691fbfa6b
hishamco <hishamco_2007@yahoo.com>
2017-03-11 19:50:00 +0300

    Use WebFormsOptions.PagesLocation in PageFactory

--- src/My.AspNetCore.WebForms/Infrastructure/PageFactory.cs
+++ src/My.AspNetCore.WebForms/Infrastructure/PageFactory.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Reflection;
 
 namespace My.AspNetCore.WebForms.Infrastructure
 {
     public class PageFactory : IPageFactory
     {
+        private readonly WebFormsOptions _webFormsOptions;
+
+        public PageFactory(IOptions<WebFormsOptions> webFormsOptions)
+        {
+            _webFormsOptions = webFormsOptions.Value;
+        }
+
         public Page CreatePage(string type)
         {
             var assembly = Assembly.GetEntryAssembly();
-            var assemblyName = assembly.GetName().Name;
-            var pagesFolder = "Pages";
-            var pageFullName = string.Join(".", assemblyName, pagesFolder, type);
-            var pageType = assembly.GetType(pageFullName);
+            var pageFullyQualifiedName = string.Join(".",
+                assembly.GetName().Name, _webFormsOptions.PagesLocation, type);
+            var pageType = assembly.GetType(pageFullyQualifiedName);
 
             if (pageType == null)
             {
